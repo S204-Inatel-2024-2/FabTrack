b09846a38a31970bae9fc3f526515a8ee6a32fa7
Felipe Silveira <felipe.s@ges.inatel.br>
2024-08-24 19:00:29 -0300

    chore: Update project URLs and include app-specific URLs

--- FabTrack/urls.py
+++ FabTrack/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('django_admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('payments/', include('payments.urls')),
+    path('admin/', include('admin_panel.urls')),
 ]
